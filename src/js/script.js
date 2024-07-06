a4d2158c119bd995548113721c86b70f7e5c414b
JoaoVitor2004 <joaovitorsouzaa28122004@gmail.com>
2024-07-05 23:01:50 -0300

    adicionar novas alterações

--- src/js/script.js
+++ src/js/script.js
@@ -66,3 +66,57 @@ buttonElement[4].addEventListener('click', () => {
     limparClasse()
 })
 buttonFechar.addEventListener('click', limparClasse)
+
+
+
+const form = document.querySelector('#form')
+const inputNome = document.querySelector('#nome')
+const inputSobrenome = document.querySelector('#sobrenome')
+const inputEmail = document.querySelector('#email')
+const inputTelefone = document.querySelector('#celular')
+const textArea = document.querySelector('#mensagem')
+
+function validarEmail(email) {
+    const validação = new RegExp(
+         /^[a-zA-Z-_]+@[a-zA-Z]+\.+[a-zA-z]/
+    )
+
+    if (validação.test(email)) {
+        return true
+    }
+    
+    return false
+}
+
+form.addEventListener('submit', (event) => {
+
+    event.preventDefault()
+
+    if (inputNome.value === '' || inputNome.value.length < 4) {
+        alert('Digite um nome valido')
+        return
+    }
+
+    if (inputSobrenome.value === '' || inputSobrenome.value.length < 4) {
+        alert('Digite um sobrenome valido')
+        return
+    }
+
+    if (!validarEmail(inputEmail.value)) {
+        alert('Digite um email valido')
+        return
+    }
+
+    if (inputTelefone.value === '' || inputTelefone.value.length < 11) {
+        alert('Digite um telefone com apenas números')
+        return
+    }
+
+    if (textArea.value === '') {
+        alert('Digite uma mensagem com apenas letras')
+        return
+    }
+
+    alert('Dados enviados')
+    form.submit()
+})
